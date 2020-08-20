030e9bc3550f95500645b1c3802df590b7502760
xinyi0526 <xinyi.luvand620@gmail.com>
2020-08-20 10:25:09 +0800

    Initial commit & Submission

---
+++ app/src/main/java/com/myapplicationdev/android/demokotlinbasic/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.myapplicationdev.android.demokotlinbasic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnGetAge.setOnClickListener {
+            val userDOB = etDOB.text.toString()
+            if(userDOB.isNotEmpty()){
+                val year = Calendar.getInstance().get(Calendar.YEAR)
+                val age = year - userDOB.toInt()
+
+                var output = ""
+                /*if(age < 18){
+                    output = "Underage"
+                }else if(age >= 18 && age < 66){
+                    output = "Young ppl"
+                }else if( age >= 66 && age < 80){
+                    output = "Middle-aged"
+                }else if (age >= 80 && age <100){
+                    output = "Elderly"
+                }else{
+                    "Long-lived elderly"
+                }*/
+                when(age){
+                    in 0..17 -> output = "Underage"
+                    in 18..65 -> output = "Young people"
+                    in 66..79 -> output = "Middle-aged"
+                    in 80..99 -> output = "Elderly"
+                    else -> {
+                        output = "Long-lived elderly"
+                    }
+                }
+                tvShowAge.text = "Your age is $age \n $output"
+            }else{
+                Toast.makeText(this,"Please enter your birth year",Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+}
